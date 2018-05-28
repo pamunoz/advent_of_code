3a46de8933c97078fd0a0bfef034d019750e9a63
pamunoz <pfariasmunoz@gmail.com>
2018-05-28 00:07:38 -0400

    add json reading

--- src/y2015/day1/day1_NotQuiteLisp.kt
+++ src/y2015/day1/day1_NotQuiteLisp.kt
@@ -1,6 +1,18 @@
 package y2015.day1
 
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.util.*
+
 
 fun main(args: Array<String>) {
-    println("Testing")
+    readFile()
+}
+
+fun readFile() {
+    val input: Scanner = Scanner(File("assets/json/inputs.json"))
+    val b: StringBuilder = StringBuilder()
+    val answer: String = input.nextLine()
+    println(answer)
 }
