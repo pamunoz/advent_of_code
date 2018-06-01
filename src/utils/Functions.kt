f687a3d22381738e9b461f3f0451f87b1bf39aa2
pamunoz <pfariasmunoz@gmail.com>
2018-06-01 16:51:46 -0400

    Add input for day 3

--- src/utils/Functions.kt
+++ src/utils/Functions.kt
@@ -4,9 +4,3 @@ import java.io.File
 
 fun inputFromFile(year: Int, day: Int) = File("assets/inputs/$year/$day").text.trim()
 
-
-fun toArrayLongString(input: String) {
-    var newInput = input
-    var count = 0
-}
-
